d393b1db7d688f6d205dc839c5d2fe143b16b807
kaiqiangren <1006042475@qq.com>
2018-08-09 11:08:24 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,6 @@ export default new Router({
       path: '/',
       name: 'index',
       component: resolve => require(['@/pages/index'], resolve)
-    },
-    {
-      path: '/three',
-      name: 'three',
-      component: resolve => require(['@/pages/three'], resolve)
-    },
-    {
-      path: '/canvas',
-      name: 'canvas',
-      component: resolve => require(['@/pages/canvas'], resolve)
-    },
-    {
-      path: '/chartTest',
-      name: 'chartTest',
-      component: resolve => require(['@/pages/chartTest'], resolve)
-    },
-    {
-      path: '/navMenu',
-      name: 'navMenu',
-      component: resolve => require(['@/pages/navMenu'], resolve)
     }
   ]
 })
