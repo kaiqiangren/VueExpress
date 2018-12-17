612508939b0755ea401fced635529c0c32b2ebc9
kaiqiangren <1006042475@qq.com>
2018-12-17 10:31:18 +0800

    debug

--- server/db/db.js
+++ server/db/db.js
@@ -7,14 +7,17 @@ const pool = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
   password : '123456',
-  database:'test'
+  database:'test1'
 });
 /*封装数据库操作方法query--用于增删改查*/
 const query=function(sql,sqlParams,callback){
+  //获取数据库的连接
   pool.getConnection(function(err,conn){
     if(err){
+      //错误回调
       callback(err,null,null);
     }else{
+      //连接成功的情况
       conn.query(sql,sqlParams,function(qerr,vals,fields){
         //释放连接
         conn.release();
