d99ffb3bf79ce2f2d6c57994813dfdfd7b7d1bde
kaiqiangren <1006042475@qq.com>
2018-12-17 16:03:11 +0800

    remove cluster

--- process.js
+++ process.js
@@ -12,7 +12,9 @@ module.exports = {
     name        : "process",
     script      : "./app.js",
     watch       : false,
-    instances  : cpus,  //文件修改后自动重启服务器
-    exec_mode : "cluster", //多集群模式
+    instances  : cpus/2,  //文件修改后自动重启服务器
+    // exec_mode : "cluster", //多集群模式
+    error_file:"log/nodejs/pm2-err.log",//错误输出日志
+    out_file:"log/nodejs/pm2-out.log" //日志
   }]
 }
